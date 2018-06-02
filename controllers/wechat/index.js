52f00c474298916d00e97daf3b3cbae4cf2183ef
HeavenDuke <trashlhc@163.com>
2018-06-02 14:11:08 +0800

    完成微信授权接口

--- controllers/wechat/index.js
+++ controllers/wechat/index.js
@@ -1,5 +1,46 @@
+var crypto = require('crypto');
 
 exports.index = function (req, res, next) {
     var ticket = req.ticket;
-    return res.json(ticket);
+    var url = decodeURI(req.query.url);
+    var nonce = (function (length) {
+        var alphabet = "qwertyuiopasdfghjklxzcvbnm1234567890";
+        var result = [];
+        for(var i = 0; i < length; i++) {
+            result.push(alphabet.charAt(Math.floor(Math.random() * (alphabet.length - 1))));
+        }
+        return result.join("");
+    })(12);
+    var timestamp = Date.now() / 1000;
+    var array = [{
+        key: "noncestr",
+        value: nonce
+    }, {
+        key: "jsapi_ticket",
+        value: ticket
+    }, {
+        key: "timestamp",
+        value: timestamp
+    }, {
+        key: "url",
+        value: url
+    }];
+    array.sort(function (item1, item2) {
+        return item1.value.toString() < item2.value.toString();
+    });
+    var result = "";
+    array.forEach(function (item) {
+        if (result !== "") {
+            result += "&";
+        }
+        result += item.key + "=" + item.value;
+    });
+    var sha1 = crypto.createHash('sha1');
+    sha1.update(result);
+    return res.json({
+        nonce: nonce,
+        timestamp: timestamp,
+        ticket: ticket,
+        signature: sha1.digest('hex')
+    });
 };
